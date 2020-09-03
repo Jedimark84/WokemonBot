ffeb8f92c81d57870a4b41cadc7615895c1fc7fd
Mark Rylander <41396220+Jedimark84@users.noreply.github.com>
2020-09-03 19:49:24 +0100

    Dev Thursday 3rd September 2020
    Dev Thursday 3rd September 2020

--- raid_function.py
+++ raid_function.py
@@ -155,8 +155,7 @@ def get_raid_participants_by_id(raid_id):
         connection.close()
         
     return result
-    
-    
+
 def list_raids():
     
     connection = db.connect()
@@ -171,8 +170,10 @@ def list_raids():
         connection.close()
         
     return result
-    
+
 def format_raid_message(raid_dict):
+    
+    # TODO: This has become a mess - I need to refactor this!
 
     if raid_dict.get('raid_datetime').date() == datetime.today().date():
         raid_datetime_string = raid_dict.get('raid_datetime').strftime("%H:%M")
@@ -184,19 +185,42 @@ def format_raid_message(raid_dict):
     participation = 'There are currently no participants for this raid\.'
     if raid_participant_dict:
         participation = ''
-        going = "*Going In Person*"
-        remote = "*Joining Remotely*"
-        invite = "*Requesting an Invite*"
-        for p in raid_participant_dict:
-            if p.get('participation_type_id') == 1:
-                going += ('\n{0}'.format(p.get('username')))
-            elif p.get('participation_type_id') == 2:
-                remote += ('\n{0}'.format(p.get('username')))
-            elif p.get('participation_type_id') == 3:
-                invite += ('\n{0}'.format(p.get('username')))
-                
-        participation += ('{0}\n{1}\n{2}'.format(going,remote,invite))
         
+        physical=False
+        remote=False
+        invite=False
+        
+        going_str = ""
+        invite_str = ""
+        remote_str = ""
+        
+        for p in raid_participant_dict:
+            
+            if p.get('participation_type_id') == 1:
+                if not physical:
+                    physical=True
+                    going_str = "*Going In Person*"
+                
+                going_str += ('\n{0}'.format(p.get('username')))
+                
+            elif p.get('participation_type_id') == 2:
+                if not remote:
+                    remote=True
+                    remote_str = "*Joining Remotely*"
+                
+                remote_str += ('\n{0}'.format(p.get('username')))
+                
+            elif p.get('participation_type_id') == 3:
+                if not invite:
+                    invite=True
+                    invite_str = "*Requesting an Invite*"
+                
+                invite_str += ('\n{0}'.format(p.get('username')))
+
+        participation += ('{0}\n{1}\n{2}'.format(going_str,remote_str,invite_str))
+    
+    # IT IS VERY IMPORTANT THAT THE MESSAGE STARTS WITH 'Raid {raid_id};'
+    # IT IS USED TO PARSE CALLBACK RESPONSES TO FIGURE OUT THE RAID ID
     return "*Raid* {0}; *Organiser:* {1}\n*Title:* {2}\n*Time and Place:* {3} \@ {4}\n\n{5}".format(
                                             raid_dict.get('raid_id'), \
                                             raid_dict.get('raid_creator_id'),
@@ -205,3 +229,146 @@ def format_raid_message(raid_dict):
                                             raid_dict.get('raid_location'),
                                             participation
                                         )
+
+def insert_message_tracking(raid_id, chat_id, message_id):
+    
+    # Connect to the database
+    connection = db.connect()
+    
+    try:
+        with connection.cursor() as cursor:
+            # Create a new record
+            sql = "INSERT INTO `message_tracking` (`raid_id`, `chat_id`, `message_id`) \
+                    VALUES ({0}, '{1}', '{2}')".format(raid_id, chat_id, message_id)
+            cursor.execute(sql)
+
+        connection.commit()
+    
+    # If there is an error then raise it to the calling function.
+    # It should get handled by the lambda_handler function.
+    #except Exception as e: raise
+        
+    finally:
+        connection.close()
+        
+def get_raid_participation_by_id(raid_id, raider_id):
+    
+    connection = db.connect()
+    
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM `raid_participants` WHERE `raid_id` = {0} AND `raider_id` = {1}".format(raid_id, raider_id)
+            cursor.execute(sql)
+            result = cursor.fetchone()
+
+    finally:
+        connection.close()
+        
+    return result
+        
+def get_raider_by_id(id):
+    
+    connection = db.connect()
+    
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM `raiders` WHERE `telegram_id` = {0}".format(id)
+            cursor.execute(sql)
+            result = cursor.fetchall()
+
+    finally:
+        connection.close()
+        
+    return result
+
+def update_raid_participation(raid_id, raider_id, participation_type_id):
+
+    # Connect to the database
+    connection = db.connect()
+    
+    try:
+        with connection.cursor() as cursor:
+            # Create a new record
+            sql = "UPDATE `raid_participants` SET `participation_type_id` = {0} WHERE (`raid_id` = {1}) and (`raider_id` = {2})".format(participation_type_id, raid_id, raider_id)
+            cursor.execute(sql)
+
+        connection.commit()
+        
+        return True
+    
+    # If there is an error then raise it to the calling function.
+    # It should get handled by the lambda_handler function.
+    #except Exception as e: raise
+        
+    finally:
+        connection.close()
+    
+def insert_raider(user_info):
+    
+    telegram_id = user_info['id']
+    username = user_info['username']
+    
+    # Connect to the database
+    connection = db.connect()
+    
+    try:
+        with connection.cursor() as cursor:
+            # Create a new record
+            sql = "INSERT INTO `raiders` (`telegram_id`, `username`) \
+                    VALUES ({0}, '{1}')".format(telegram_id, username)
+            cursor.execute(sql)
+
+        connection.commit()
+    
+    # If there is an error then raise it to the calling function.
+    # It should get handled by the lambda_handler function.
+    #except Exception as e: raise
+        
+    finally:
+        connection.close()
+
+def insert_raid_participation(raid_id, raider_id, participation_type_id):
+    
+    # Connect to the database
+    connection = db.connect()
+    
+    try:
+        with connection.cursor() as cursor:
+            # Create a new record
+            sql = "INSERT INTO `raid_participants` (`raid_id`, `raider_id`, `participation_type_id`, `party_count`) \
+                    VALUES ({0}, {1}, {2}, {3})".format(raid_id, raider_id, participation_type_id, 1)
+            cursor.execute(sql)
+
+        connection.commit()
+        
+        return True
+    
+    # If there is an error then raise it to the calling function.
+    # It should get handled by the lambda_handler function.
+    #except Exception as e: raise
+        
+    finally:
+        connection.close()
+
+def join_raid(from_object, raid_id, participation_type_id):
+    
+    # Step 1: Verify the user exists in the raiders table
+    #.        If they don't then create them!
+    if not get_raider_by_id(from_object['id']):
+        insert_raider(from_object)
+        
+    # Step 2: See if the user is already participating in the raid
+    #         They may have changed their participation type
+    #         Or just sending a duplicate request which we should ignore
+    #         If they are not already participating - then add them
+    p = get_raid_participation_by_id(raid_id, from_object['id'])
+    if not p:
+        return insert_raid_participation(raid_id, from_object['id'], participation_type_id)
+    else:
+        if p.get('participation_type_id') == int(participation_type_id):
+            return False
+        else:
+            return update_raid_participation(raid_id, from_object['id'], participation_type_id)
+    
+    # We should never get here!
+    return False
